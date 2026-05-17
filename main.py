6646302ffdfe04d0e9a71a7083b80205924a8685
radek-vlach <wllach@post.cz>
2026-05-17 10:29:49 +0200

    Import knihoven

---
+++ main.py
@@ -0,0 +1,6 @@
+import csv  # Načtení modulu pro zápis dat do souboru CSV.
+import sys  # Načtení modulu pro práci s argumenty.
+from urllib.parse import urljoin  # Import funkce pro skládání URL adres.
+
+import requests  # Import knihovny pro stahování webových stránek.
+from bs4 import BeautifulSoup  # Import nástroje pro parsování HTML.
